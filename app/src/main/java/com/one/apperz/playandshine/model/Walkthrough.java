d9fc0acea0e944e34aaa395658da7a8fe1aab061
varundutia <51284086+varundutia@users.noreply.github.com>
2020-09-01 09:02:54 +0530

    walk
    walkthrough

---
+++ app/src/main/java/com/one/apperz/playandshine/model/Walkthrough.java
@@ -0,0 +1,32 @@
+package com.one.apperz.playandshine.model;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.one.apperz.playandshine.R;
+
+import uk.co.samuelwall.materialtaptargetprompt.MaterialTapTargetPrompt;
+
+public class Walkthrough {
+    public Walkthrough(View v, Activity activity,String f,String s){
+
+        new MaterialTapTargetPrompt.Builder(activity)
+                .setTarget(v.getId())
+                .setPrimaryText(f)
+                .setSecondaryText(s)
+                .setPromptStateChangeListener(new MaterialTapTargetPrompt.PromptStateChangeListener()
+                {
+                    @Override
+                    public void onPromptStateChanged(MaterialTapTargetPrompt prompt, int state)
+                    {
+                        if (state == MaterialTapTargetPrompt.STATE_FOCAL_PRESSED)
+                        {
+                            // User has pressed the prompt target
+                            Toast.makeText(activity.getApplicationContext(),"done",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                })
+                .show();
+    }
+}
